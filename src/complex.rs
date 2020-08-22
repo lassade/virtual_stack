1c1cfa1defe9bf61880f87c03d610ec312a8ddd0
lassade <lassade@localhost.localdomain>
2020-08-22 16:57:39 -0300

    virtualized stack runs while normal overflows!

--- src/complex.rs
+++ src/complex.rs
@@ -1,37 +1,28 @@
 use std::alloc::{alloc, dealloc, Layout};
 use std::mem::MaybeUninit;
 use std::ptr::copy_nonoverlapping;
+use test::black_box;
 
 #[derive(Copy, Clone)]
-pub struct Slab {
+struct Slab {
     sp: *const u8,
     heap: *mut u8,
     size: usize,
 }
 
-struct Fib {}
+struct Rec {}
 
-impl Fib {
-    fn new() -> Self {
-        Self {}
-    }
-
-    #[inline(never)]
-    fn __calc_inner(&mut self, n: usize, s: Slab) -> usize {
-        // big stack allocation
-        test::black_box(unsafe { MaybeUninit::<[u8; 180]>::uninit().assume_init() });
-
-        let v;
-        if (n == 0) || (n == 1) {
-            v = n
-        } else {
-            v = self.calc(n - 1, Some(s)) + self.calc(n - 2, Some(s));
+impl Rec {
+    #[inline(always)]
+    fn __rec_inner(n: usize, s: Slab) {
+        black_box(unsafe { MaybeUninit::<[u8; 256]>::uninit().assume_init() });
+        if n != 0 {
+            black_box(Self::rec(n - 1, Some(s)))
         }
-        v
     }
 
     #[inline(never)]
-    fn calc(&mut self, n: usize, s: Option<Slab>) -> usize {
+    fn rec(n: usize, s: Option<Slab>) {
         let __v; // Return value
         let mut __s; // Allocated or inherited stack slab,
         let mut __discard = false; // Owns the stack `Slab` thus need to dealloc it at the end
@@ -63,6 +54,7 @@ impl Fib {
                     __s = slab;
                 }
             } else {
+                asm!("nop");
                 let size = 64 * 1024;
                 let layout = Layout::from_size_align_unchecked(size, 32);
                 let heap = alloc(layout);
@@ -76,24 +68,38 @@ impl Fib {
                 // Activate stack slab
                 asm!("mov rsp, {vsp}", vsp = in(reg) vsp,);
             }
+
+            // asm!(
+            //     "push {0}",
+            //     "push {1}",
+            //     "push {2}",
+            //     in(reg) __s.sp, in(reg) __s.heap, in(reg) __s.size,
+            // );
         }
 
         // Call function
-        __v = self.__calc_inner(n, __s);
+        __v = Self::__rec_inner(n, __s);
 
         unsafe {
+            // asm!(
+            //     "pop {2}",
+            //     "pop {1}",
+            //     "pop {0}",
+            //     out(reg) __s.sp, out(reg) __s.heap, out(reg) __s.size,
+            // );
+
             // Stack `Slab` changed!
             if __discard {
-                let layout = Layout::from_size_align_unchecked(__s.size, 32);
-                dealloc(__s.heap, layout);
-
                 // Restore to previous `Slab` or the frame in the program stack
                 asm!(
-                    //"mov {v}, {v}", // Make the sp reg not the same as the return value reg
+                    "nop", // Make the sp reg not the same as the return value reg
                     "mov rsp, {sp}",
                     //v = in(reg) __v,
                     sp = in(reg) __s.sp
                 );
+
+                let layout = Layout::from_size_align_unchecked(__s.size, 32);
+                dealloc(__s.heap, layout);
             }
         }
 
@@ -102,7 +108,6 @@ impl Fib {
     }
 }
 
-pub fn exec() {
-    let f = test::black_box(Fib::new().calc(40, None));
-    println!("fib(40): {}", f);
+pub fn rec(n: usize) {
+    black_box(Rec::rec(n, None));
 }
--- src/main.rs
+++ src/main.rs
@@ -2,91 +2,32 @@
 #![feature(asm)]
 #![feature(ptr_offset_from)]
 
-// use std::alloc::{alloc, dealloc, Layout};
-use std::mem::{ManuallyDrop, MaybeUninit};
-
 extern crate test;
+
+use std::mem::MaybeUninit;
+use test::black_box;
 
 mod complex;
 mod simple;
 
 fn main() {
-    println!("fib(40): {}", fib(40));
-    println!("normal stack done!");
+    const N: usize = 100_000;
     // simple::exec();
     // println!("simple done!");
-    complex::exec();
-    println!("virtualized stack done!");
+
+    println!("virtualized stack");
+    complex::rec(N);
+    println!("done!");
+    println!("-----------------------------------");
+    println!("normal stack");
+    rec(N);
+    println!("done!");
 }
 
 #[inline(never)]
-fn fib(n: usize) -> usize {
-    // big stack allocation
-    test::black_box(unsafe { MaybeUninit::<[u8; 180]>::uninit().assume_init() });
-
-    if (n == 0) || (n == 1) {
-        n
-    } else {
-        fib(n - 1) + fib(n - 2)
+pub fn rec(n: usize) {
+    black_box(unsafe { MaybeUninit::<[u8; 256]>::uninit().assume_init() });
+    if n != 0 {
+        black_box(rec(n - 1))
     }
 }
-
-// fn main() {
-//     const SIZE: usize = 2 * 1024 * 1024;
-//     let layout = Layout::from_size_align(SIZE, 8).unwrap();
-//     let stack_ptr = unsafe { alloc(layout) };
-
-//     let vsp = unsafe { stack_ptr.add(SIZE) };
-//     let mut sp: *const usize = std::ptr::null();
-
-//     unsafe {
-//         asm!(
-//             // "nop",
-//             // "nop",
-//             "mov {sp}, rsp",
-//             "mov rsp, {vsp}",
-//             // "nop",
-//             // "nop",
-//             sp = out(reg) sp,
-//             vsp = in(reg) vsp,
-//         );
-//     }
-
-//     fib(10);
-
-//     unsafe {
-//         asm!(
-//             "mov rsp, {sp}",
-//             sp = in(reg) sp,
-//         );
-//     }
-
-//     unsafe {
-//         dealloc(stack_ptr, layout);
-//     }
-// }
-
-// fn main() {
-//     unsafe {
-//         let layout = Layout::from_size_align(64, 8).unwrap();
-//         let stack_ptr = alloc(layout);
-
-//         let vsp = stack_ptr.add(64);
-//         let mut sp: *const usize = std::ptr::null();
-
-//         asm!(
-//             "mov {sp}, rsp",
-//             "mov rsp, {vsp}",
-//             "push {number}",
-//             "mov rsp, {sp}",
-//             sp = out(reg) sp,
-//             vsp = in(reg) vsp,
-//             number = const 5,
-//         );
-
-//         let v7 = *(stack_ptr as *const usize).add(7);
-//         println!("{}", v7);
-
-//         dealloc(stack_ptr, layout)
-//     }
-// }
